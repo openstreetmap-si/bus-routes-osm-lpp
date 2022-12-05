1a33d427fc49711ff47483f0ed6e9d64dee7aba1
Štefan Baebler <stefan.baebler@gmail.com>
2022-12-05 18:53:41 +0100

    Scrape LPP Routes and Stops data

---
+++ README.md
@@ -0,0 +1,9 @@
+# LPP bus routes in OpenStreetMap
+
+## Development
+
+1. `python3 -m venv venv` or `virtualenv -p python3 venv`
+2. `source venv/bin/activate`
+3. `pip install -r requirements.txt`
+4. `python update.py`
+
---
+++ update.py
@@ -0,0 +1,83 @@
+#!./venv/bin/python
+
+import requests
+from bs4 import BeautifulSoup
+import re
+import pandas as pd
+
+BaseURL = "https://www.lpp.si/sites/default/files/lpp_vozniredi/iskalnik/index.php"
+
+
+def getLppStops():
+    page = requests.get(BaseURL)
+    # print(page)
+    page.raise_for_status()
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    stopTextRegex = r"^(.*) \(([0-9]{6,6})\)$"
+
+    selectListStops = soup.find("select", {"id": "stop"})
+    # print(selectListStops)
+    stopsArray = []
+    stopsOptions = selectListStops.find_all('option')
+    for stopOption in stopsOptions:
+        val = stopOption['value']
+        if val == "":
+            continue
+        text = stopOption.text.strip()
+        match = re.match(stopTextRegex, text)
+        if match == None:
+            print(
+                f"Unexpected text '{text}' in stop option, not matching regex '{stopTextRegex}', aborting.")
+            raise
+
+        name = text[:-len(val)-3]
+        stopsArray.append([val, name])
+
+    df = pd.DataFrame(stopsArray, columns=['id', 'name'])
+    df.set_index('id', inplace=True)
+    df.sort_values(by=['name'], inplace=True)
+    print(df)
+    df.to_csv('data/lpp/stops.csv')
+
+
+def getLppLines():
+    page = requests.get(BaseURL)
+    page.raise_for_status()
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    # 11B - BEŽIGRAD (Železna) - ZALOG
+    lineTextRegex = r"^([0-9A-Z]{1,3}) - ((.*) - (.*))$"
+
+    selectListLines = soup.find("select", {"id": "line"})
+    linesArray = []
+    linesOptions = selectListLines.find_all('option')
+    for stopOption in linesOptions:
+        val = stopOption['value']
+        if val == "":
+            continue
+        text = stopOption.text.strip()
+        match = re.match(lineTextRegex, text)
+        if match == None:
+            print(
+                f"Unexpected text '{text}' in stop option, not matching regex '{lineTextRegex}', aborting.")
+            raise
+
+        line = match.group(1).strip()
+        name = match.group(2).strip()
+        nameFrom = match.group(3).strip()
+        nameTo = match.group(4).strip()
+        linesArray.append([val, line, name, nameFrom, nameTo])
+
+    df = pd.DataFrame(linesArray, columns=[
+                      'id', 'line', 'name', 'nameFrom', 'nameTo'])
+    df.set_index('id', inplace=True)
+    # df.sort_values(by=['line'], inplace=True)
+    print(df)
+    df.to_csv('data/lpp/lines.csv')
+
+
+if __name__ == "__main__":
+
+    getLppStops()
+    getLppLines()
